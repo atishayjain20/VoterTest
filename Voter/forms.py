12a5054f1651cc91b9ccd5116994b2ae4b0d23d8
nacho <bharatchhipa3220@gmail.com>
2021-05-29 11:16:24 +0530

    Initial commit

---
+++ Voter/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    aadhar = forms.CharField(max_length=12)
+    contactno = forms.CharField(max_length=10, required=False)
+
+    class Meta:
+        model = User
+        # fields = '__all__'
+        fields = ['username',  'password1',
+                  'password2', 'aadhar', 'email', 'contactno']
+        exclude = ['contactno']
+
+
+# class registrationTest(UserRegisterForm):
+#     class Meta:
+#         model = User
+#         exclude = ['contactno']
